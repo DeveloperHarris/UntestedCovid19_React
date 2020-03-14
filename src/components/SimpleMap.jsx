895c78ce2a8af34bba7340176c22758dc07e97a2
Harris R <harris.rothaermel@gmail.com>
2020-03-13 21:47:47 -0600

    Basic Map + Firebase

---
+++ src/components/SimpleMap.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 37.0902,
+      lng: -95.7129
+    },
+    zoom: 3
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: "100vh", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyDGt6HmbX6CXrxxFJGtoR69vxI7JyNIg3E" }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        ></GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default SimpleMap;
