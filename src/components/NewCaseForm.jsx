9008f53ad0beb6ae93a37e714c6a68fce309cc8d
Harris R <harris.rothaermel@gmail.com>
2020-03-18 15:40:57 -0600

    Prepare for open source

--- src/components/NewCaseForm.jsx
+++ src/components/NewCaseForm.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
 
 class NewCaseForm extends Component {
   state = {
@@ -13,9 +15,11 @@ class NewCaseForm extends Component {
     sore_throat: false,
     phone: "",
     code: "",
-    timestamp: 0,
-    lat: 40.235119,
-    lng: -111.662193
+    timestamp: Date.now(),
+    lat: 0.0,
+    lng: 0.0,
+    sent: false,
+    submitted: false
   };
 
   //todo implement zipcode
@@ -26,6 +30,8 @@ class NewCaseForm extends Component {
 
   sendPhoneCode = () => {
     if (!this.state.phone) return false;
+
+    this.setState({ sent: true });
 
     let data = new URLSearchParams();
     data.append("phone", this.state.phone);
@@ -40,12 +46,16 @@ class NewCaseForm extends Component {
   };
 
   submit = () => {
-    if (!this.state.phone) return false;
+    if (!this.state.phone || !this.state.code) return false; // no verification
+    if (!this.hasSymptom()) return false;
+    if (!this.state.sent) return false;
 
-    this.setState({ timestamp: Date.now() }); // doesn't do it in time because of async update TODO: fix
+    this.setState({ submitted: true });
 
     let data = new URLSearchParams();
     for (let property in this.state) {
+      // TODO: explicity append what you need, not everything
+      if (property === "sent" || property === "submitted") continue;
       data.append(property, this.state[property]);
     }
 
@@ -56,6 +66,24 @@ class NewCaseForm extends Component {
         body: data
       }
     );
+  };
+
+  hasSymptom = () => {
+    if (
+      !(
+        this.state.short_of_breath ||
+        this.state.fever ||
+        this.state.headache ||
+        this.state.runny_nose ||
+        this.state.diarrhea ||
+        this.state.cough ||
+        this.state.body_aches ||
+        this.state.congestion ||
+        this.state.sore_throat
+      )
+    ) {
+      return false;
+    } else return true;
   };
 
   getLocation = () => {
@@ -72,12 +100,16 @@ class NewCaseForm extends Component {
     });
   };
 
+  setPhone = value => {
+    this.setState({ phone: value });
+  };
+
   // TODO: refactor this so we don't need to repeat so much code
   render() {
     return (
       <React.Fragment>
         <div className="newCaseForm">
-          <p>Mark Your Symptoms</p>
+          <p className="newCaseHeader">Mark Your Symptoms</p>
           <label>
             <input
               type="checkbox"
@@ -85,43 +117,7 @@ class NewCaseForm extends Component {
               value="short of breath"
               onClick={() => this.handleChange("short_of_breath")}
             />
-            <p>Short of Breath</p>
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              id="fever"
-              value="fever"
-              onClick={() => this.handleChange("fever")}
-            />
-            <p>Fever</p>
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              id="headache"
-              value="headache"
-              onClick={() => this.handleChange("headache")}
-            />
-            <p>Headache</p>
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              id="runny_nose"
-              value="runny_nose"
-              onClick={() => this.handleChange("runny_nose")}
-            />
-            <p>Runny Nose</p>
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              id="diarrhea"
-              value="diarrhea"
-              onClick={() => this.handleChange("diarrhea")}
-            />
-            <p>Diarrhea</p>
+            <p className="inputText">Short of Breath</p>
           </label>
           <label>
             <input
@@ -130,25 +126,7 @@ class NewCaseForm extends Component {
               value="cough"
               onClick={() => this.handleChange("cough")}
             />
-            <p>Cough</p>
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              id="body_aches"
-              value="body_aches"
-              onClick={() => this.handleChange("body_aches")}
-            />
-            <p>Body Aches</p>
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              id="congestion"
-              value="congestion"
-              onClick={() => this.handleChange("congestion")}
-            />
-            <p>Congestion</p>
+            <p className="inputText">Cough</p>
           </label>
           <label>
             <input
@@ -157,44 +135,128 @@ class NewCaseForm extends Component {
               value="sore_throat"
               onClick={() => this.handleChange("sore_throat")}
             />
-            <p>Sore Throat</p>
+            <p className="inputText">Sore Throat</p>
           </label>
-          <input
-            type="submit"
-            value="Use GPS"
-            className="sendPhoneCode"
-            onClick={this.getLocation}
-          />
-          <div className="phoneVerificationForm">
-            <p>Phone Number (To Verify)</p>
+          <label>
             <input
-              type="text"
-              id="fname"
-              name="fname"
-              onChange={event => this.setState({ phone: event.target.value })}
+              type="checkbox"
+              id="runny_nose"
+              value="runny_nose"
+              onClick={() => this.handleChange("runny_nose")}
             />
+            <p className="inputText">Runny Nose</p>
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              id="congestion"
+              value="congestion"
+              onClick={() => this.handleChange("congestion")}
+            />
+            <p className="inputText">Congestion</p>
+          </label>
+
+          <label>
+            <input
+              type="checkbox"
+              id="headache"
+              value="headache"
+              onClick={() => this.handleChange("headache")}
+            />
+            <p className="inputText">Headache</p>
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              id="fever"
+              value="fever"
+              onClick={() => this.handleChange("fever")}
+            />
+            <p className="inputText">Fever</p>
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              id="diarrhea"
+              value="diarrhea"
+              onClick={() => this.handleChange("diarrhea")}
+            />
+            <p className="inputText">Diarrhea</p>
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              id="body_aches"
+              value="body_aches"
+              onClick={() => this.handleChange("body_aches")}
+            />
+            <p className="inputText">Body Aches</p>
+          </label>
+
+          <div className="spacer" />
+          <div className="locationDivs">
+            <p className="addLocation">Add Location: </p>
+            <div className="active button" onClick={this.getLocation}>
+              Use GPS
+            </div>
           </div>
-          <input
-            type="submit"
-            value="Send Code"
-            className="sendPhoneCode"
-            onClick={this.sendPhoneCode}
-          />
+          <div className="location">
+            <p className="locationText">
+              Latitude: {this.state.lat.toPrecision(4)}
+            </p>
+            <p className="locationText">
+              Longitude: {this.state.lng.toPrecision(4)}
+            </p>
+          </div>
+
+          <div className="spacer" />
           <div className="phoneVerificationForm">
-            <p>Verification Code</p>
+            <p className="phoneNumber">Verify Phone Number:</p>
+
+            <div className="spacer" />
+
+            <PhoneInput
+              placeholder="Enter phone number"
+              defaultCountry="US"
+              value={this.state.phone}
+              onChange={this.setPhone}
+              className="phoneInput"
+            />
+            <div className="spacer" />
+            <div
+              className="active button sendPhoneCode"
+              onClick={this.sendPhoneCode}
+            >
+              Send Code
+            </div>
+            <div className="response">
+              <p className="responseText">{this.state.sent ? "Sent" : ""}</p>
+            </div>
+          </div>
+          <div className="spacer" />
+          <div className="phoneVerificationCheck">
+            <p className="verificationCode">Verification Code:</p>
             <input
               type="text"
-              id="fname"
-              name="fname"
+              id="verificationCode"
+              name="verificationCode"
               onChange={event => this.setState({ code: event.target.value })}
             />
+            <div className="spacer" />
+            {this.state.phone &&
+            this.state.sent &&
+            this.state.code &&
+            this.hasSymptom() ? (
+              <div className="active button" onClick={this.submit}>
+                Submit
+              </div>
+            ) : (
+              <div className="disabled button">Submit</div>
+            )}
+            <p className="responseText">
+              {this.state.submitted ? "Submitted" : ""}
+            </p>
           </div>
-          <input
-            type="submit"
-            value="Submit"
-            className="sendPhoneCode"
-            onClick={this.submit}
-          />
         </div>
       </React.Fragment>
     );
